44121ce0e0607c4b924d3923e8e9ee1ae12b74f1
a18090 <a18090@live.cn>
2024-03-05 15:29:33 +0800

    Update ffmpeg-path.js
    system native ffmpeg

--- packages/extractor/src/extract/utils/ffmpeg-path.js
+++ packages/extractor/src/extract/utils/ffmpeg-path.js
@@ -4,21 +4,11 @@ const log = require('@home-gallery/logger')('extractor.ffmpeg');
 const { getNativeCommand } = require('./native-command')
 
 const getFfmpegPath = config => {
-  if (config?.extractor?.useNative?.includes('ffmpeg')) {
-    log.debug(`Use native system command ffmpeg`)
     return getNativeCommand('ffmpeg')
-  } else {
-    return require('@ffmpeg-installer/ffmpeg').path
-  }
 }
 
 const getFprobePath = config => {
-  if (config?.extractor?.useNative?.includes('ffprobe')) {
-    log.debug(`Use native system command ffprobe`)
     return getNativeCommand('ffprobe')
-  } else {
-    return require('@ffprobe-installer/ffprobe').path
-  }
 }
 
 module.exports = {
